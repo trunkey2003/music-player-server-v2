1aaa1ff6a9d07d84f0f7e87e5d3bb919167507a7
trunkey2007 <hoquanglam090@gmail.com>
2022-01-10 13:55:17 +0700

    fix cors

--- app.js
+++ app.js
@@ -33,6 +33,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methos', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, *');
+    next();
+});
+
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
 
