65ee3e6ca00adc90665a8a6e771726039e6b662e
trunkey2007 <hoquanglam090@gmail.com>
2022-01-14 00:37:57 +0700

    fix get user song playlist_id

--- controllers/api-user.controller.js
+++ controllers/api-user.controller.js
@@ -146,7 +146,7 @@ class ApiUserController {
     }
 
     getUserSongs(req, res) {
-        const sql = `SELECT us.name, us.singer, us.path, us.image, us.song_id AS songid, us.user_id AS userid FROM user_songs as us INNER JOIN users ON users.user_name = '${req.params.username}' AND users.user_id = us.user_id AND us.playlist_id = users.user_id`;
+        const sql = `SELECT us.name, us.singer, us.path, us.image, us.song_id AS songid, us.user_id AS userid, us.playlist_id AS playlistid FROM user_songs as us INNER JOIN users ON users.user_name = '${req.params.username}' AND users.user_id = us.user_id AND us.playlist_id = users.user_id`;
         db.query(sql, (err, result) => {
             if (err) {
                 throw err;
@@ -156,7 +156,7 @@ class ApiUserController {
     }
 
     getAllUserSongs(req, res){
-        const sql = `SELECT us.name, us.singer, us.path, us.image, us.song_id AS songid, us.user_id AS userid FROM user_songs as us INNER JOIN users ON users.user_name = '${req.params.username}' AND users.user_id = us.user_id`;
+        const sql = `SELECT us.name, us.singer, us.path, us.image, us.song_id AS songid, us.user_id AS userid, us.playlist_id AS playlistid FROM user_songs as us INNER JOIN users ON users.user_name = '${req.params.username}' AND users.user_id = us.user_id`;
         db.query(sql, (err, result) => {
             if (err) {
                 throw err;
